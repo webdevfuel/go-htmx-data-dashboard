4bfc4c8ff52ae5ed03c5160bd8f49e88a6d093c6
Emanuel Turis <hello@emanuelturis.com>
2024-10-19 22:05:42 +0300

    add server with go and templ

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/webdevfuel/go-htmx-data-dashboard
 
 go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.1.0
+
+require github.com/a-h/templ v0.2.778 // indirect
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["view/**/*.{html,js,templ}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
